1304e26064da2b992e51b68e2bc3723d20b49589
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-08 13:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function Employee(name, age, jobTitle) {}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+// Define the Person class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+// Define the Employee class that inherits from Person
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    // Call the super constructor of Person
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
